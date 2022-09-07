c09ad73a09a3e2aec7ceb07c38551e04d0fe6b66
hoon4172 <gns4172@naver.com>
2022-09-07 11:18:02 +0900

    “first-commit”

---
+++ src/modules/counter.js
@@ -0,0 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+
+//액션타입 정의
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+//액션생성 함수
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+//초기상태
+const initialState = {
+  number: 0,
+};
+
+//리듀서함수
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+export default counter;
